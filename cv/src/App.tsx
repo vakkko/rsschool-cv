c6bb3dfd7b5835dc0a36a844141938e4176185a1
Vladimer Gabisonia <vakogabo44@gmail.com>
2026-02-11 22:33:11 +0400

    feat: configure mui

--- cv/src/App.tsx
+++ cv/src/App.tsx
@@ -1,5 +1,16 @@
-function App() {
-  return <></>;
-}
+import React from "react";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import { ThemeProvider } from "@mui/material/styles";
+
+import theme from "./theme";
+
+const App: React.FC = () => {
+  return <ThemeProvider theme={theme}></ThemeProvider>;
+};
 
 export default App;
---
+++ cv/src/theme/index.ts
@@ -0,0 +1,21 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Roboto, Arial, sans-serif",
+    h1: {
+      fontSize: "2.5rem",
+      fontWeight: 700,
+    },
+    h2: {
+      fontSize: "2rem",
+      fontWeight: 500,
+    },
+    body1: {
+      fontSize: "1rem",
+      fontWeight: 400,
+    },
+  },
+});
+
+export default theme;
